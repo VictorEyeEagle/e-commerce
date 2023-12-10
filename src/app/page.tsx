ebf82ebb88ce5866b03e25880067fe0a1611e15f
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-10 02:01:44 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,12 @@ export default function Home() {
       <header className="topo">
         <Image className='logo' src={logo} alt="Logo" />
         <input className='pesquisar' placeholder='Pesquisar na e-commerce' />
-        <div className='carrinho'><img className='carrinhoicon' src={"https://cdn-icons-png.flaticon.com/128/2098/2098575.png"} /><p>carrinho</p></div>
+        <div className='carrinho'>
+          <a href='www.youtube.com' className='carrinho-link'>
+            <img className='carrinhoicon' src={"https://cdn-icons-png.flaticon.com/128/2098/2098575.png"} alt="Carrinho" />
+            <p>carrinho</p>
+          </a>
+        </div>
       </header>
     </div>
   )
