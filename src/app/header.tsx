6e4b8987a765dc6f5062e43e9d272b20b90b86ff
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-22 13:06:06 -0300

    .

---
+++ src/app/header.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import logo from './Logo-ecommerce.png'
+import { Button, Badge, IconButton } from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import "./estilos/Header.css"
+
+function Header() {
+  return (
+    <header className="topo">
+      <Image className='logo' src={logo} alt="Logo" />
+      <input className='pesquisar' placeholder='Pesquisar na e-commerce' />
+      <IconButton className='carrinho' aria-label="show cart items" color="inherit">
+        <Badge badgeContent={1} color="error">
+          <ShoppingCartIcon sx={{ fontSize: 40 }} />
+        </Badge>
+      </IconButton>
+      <Button variant="contained" color="secondary">
+        Logar
+      </Button>
+    </header>
+  )
+}
+
+export default Header
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,14 @@
-import Image from 'next/image'
-import logo from './Logo-ecommerce.png'
-import "./Home.css"
 import React from 'react'
-import { Button, Badge, IconButton } from '@mui/material'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import Header from './header'
+import Main from './main'
 
-
-export default function Home() {
+function Home() {
   return (
     <div className="container">
-      <header className="topo">
-        <Image className='logo' src={logo} alt="Logo" />
-        <input className='pesquisar' placeholder='Pesquisar na e-commerce' />
-        <IconButton className='carrinho' aria-label="show cart items" color="inherit">
-          <Badge badgeContent={1} color="error">
-            <ShoppingCartIcon sx={{ fontSize: 40 }} />
-          </Badge>
-        </IconButton>
-        <Button variant="contained" color="secondary">
-          Logar
-        </Button>
-      </header>
+      <Header />
+      <Main />
     </div>
   )
 }
+
+export default Home
