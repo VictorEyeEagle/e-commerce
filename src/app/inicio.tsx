5a2e5a821364745f69a8101578be81319404b45c
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-22 14:45:19 -0300

    .

---
+++ src/app/inicio.tsx
@@ -0,0 +1,113 @@
+import React from 'react'
+import "./estilos/inicio.css"
+
+function Main() {
+    return (
+        <main>
+            <div className="categorias">
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+                <button>
+                    <div className='cat'>
+                        <img className='img-cat' src='https://images.tcdn.com.br/img/img_prod/1189051/noticia_76899846563fcffa696315.png' />
+                        <p>Roupas Femininas</p>
+                    </div>
+                </button>
+
+                {/* Adicione mais elementos 'cat' conforme necessário */}
+            </div>
+            <div className='produtos'>
+            </div>
+        </main>
+    )
+}
+
+export default Main
