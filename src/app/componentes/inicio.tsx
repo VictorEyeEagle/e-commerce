5617168a7d095fc65960ee51d379c19998af671f
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-27 19:43:18 -0300

    .

--- src/app/componentes/inicio.tsx
+++ src/app/componentes/inicio.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./estilos/inicio.css"
 import Link from 'next/link'
-
+import Rating from '@mui/material/Rating'
 
 function Inicio() {
     return (
@@ -121,169 +121,21 @@ function Inicio() {
 
             <div className='tendencia'><h1>TENDÊNCIAS DO DIA</h1></div>
             <div className='produtos'>
-                <Link href="/pagproduto">
-                    <button>
-                        <div className='produto'>
-                            <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                            <p>KIT COM 10 CUECAS</p>
-                            <p className='preco'>R$30,90</p>
-                            <p className='vendidos'>350 vendidos</p>
-                        </div>
-                    </button>
-                </Link>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
+                {[...Array(20)].map((_, index) => (
+                    <div key={index} className='produto'>
+                        <Link href={`/pagproduto`}>
+                            <div className='link-produto'>
+                                <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' alt={`Produto ${index + 1}`} />
+                                <p>KIT COM 10 CUECAS</p>
+                                <p className='preco'>R$30,90</p>
+                                <Rating className='classif' name={`rating-${index}`} defaultValue={4.5} precision={0.5} readOnly />
+                                <p className='vendidos'>350 vendidos</p>
+                            </div>
+                        </Link>
                     </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
+                ))}
             </div>
+
         </main>
     )
 }
--- src/app/componentes/produto.tsx
+++ src/app/componentes/produto.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./estilos/produto.css"
+import Link from 'next/link'
+import Rating from '@mui/material/Rating'
 
 function Produto() {
     return (
@@ -7,174 +9,27 @@ function Produto() {
 
             <h1>Produto</h1>
             <div className='produtodetail'>
-            <img className='produtodetail-foto ' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg'/>
-
+                <img className='produtodetail-foto ' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
             </div>
-
 
             <div className='tendencia'><h1>TENDÊNCIAS DO DIA</h1></div>
             <div className='produtos'>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
+                {[...Array(20)].map((_, index) => (
+                    <div key={index} className='produto'>
+                        <Link href={`/pagproduto`}>
+                            <div className='link-produto'>
+                                <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' alt={`Produto ${index + 1}`} />
+                                <p>KIT COM 10 CUECAS</p>
+                                <p className='preco'>R$30,90</p>
+                                <Rating className='classif' name={`rating-${index}`} defaultValue={4.5} precision={0.5} readOnly />
+                                <p className='vendidos'>350 vendidos</p>
+                            </div>
+                        </Link>
                     </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
-                <button>
-                    <div className='produto'>
-                        <img className='img-produto' src='https://images.tcdn.com.br/img/img_prod/633596/kit_40_cuecas_boxer_men_kit_atacado_kit_revenda_343_1_20200209134533.jpg' />
-                        <p>KIT COM 10 CUECAS</p>
-                        <p className='preco'>R$30,90</p>
-                        <p className='vendidos'>350 vendidos</p>
-                    </div>
-                </button>
+                ))}
             </div>
+
+
         </main>
     )
 }
