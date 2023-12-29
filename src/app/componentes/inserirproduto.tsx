e6453bd602ee5db7d9aa212c59f1f22ef8f3acf1
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-29 12:27:16 -0300

    .

---
+++ src/app/componentes/inserirproduto.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./estilos/inserirproduto.css"
+
+function InserirProduto() {
+    return (
+        <div className='inserirvenda-info'>
+            <p>inserir produto</p>
+        </div>
+    )
+}
+
+export default InserirProduto
---
+++ src/app/inserirProd/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../componentes/header'
+import InserirProduto from '../componentes/inserirproduto'
+
+function InserirProdutopage() {
+  return (
+    <div className="container">
+      <Header />
+      <InserirProduto />
+    </div>
+  )
+}
+
+export default InserirProdutopage
