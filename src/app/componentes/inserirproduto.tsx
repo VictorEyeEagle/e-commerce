a6551713ca439759af45aed6f56ce06c6e342a2d
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-30 08:49:44 -0300

    .

--- src/app/componentes/inserirproduto.tsx
+++ src/app/componentes/inserirproduto.tsx
@@ -8,11 +8,11 @@ function InserirProduto() {
   return (
     <div className='inserirvenda-info'>
       <h1>Inserir produto</h1>
-      <input type="text" placeholder="URL da Imagem" className='imagem-info inputs' /><br/>
-      <input placeholder="Nome" className='nome-info inputs' /><br/>
-      <input placeholder="Preço" className='preco-info inputs' /><br/>
-      <input placeholder="Descrição" className='desc-info inputs' /><br/>
-      <button>Enviar</button>
+      <input type="text" placeholder="URL da Imagem" className='imagem-info inputs' /><br />
+      <input placeholder="Nome" className='nome-info inputs' /><br />
+      <input placeholder="Preço" className='preco-info inputs' /><br />
+      <input placeholder="Descrição" className='desc-info inputs' /><br />
+      <button className='enviarvenda-info'>Publicar</button>
     </div>
   );
 }
