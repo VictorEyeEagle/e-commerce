ec27ef3db590076e82137fc6bba91873ea7ed4f8
VictorEyeEagle <victor100bol@hotmail.com>
2023-12-29 22:08:15 -0300

    .

--- src/app/componentes/inserirproduto.tsx
+++ src/app/componentes/inserirproduto.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
 import "./estilos/inserirproduto.css"
 
 function InserirProduto() {
-    return (
-        <div className='inserirvenda-info'>
-            <p>inserir produto</p>
-        </div>
-    )
+
+  return (
+    <div className='inserirvenda-info'>
+      <h1>Inserir produto</h1>
+      <input type="text" placeholder="URL da Imagem" className='imagem-info inputs' /><br/>
+      <input placeholder="Nome" className='nome-info inputs' /><br/>
+      <input placeholder="Preço" className='preco-info inputs' /><br/>
+      <input placeholder="Descrição" className='desc-info inputs' /><br/>
+      <button>Enviar</button>
+    </div>
+  );
 }
 
-export default InserirProduto
+export default InserirProduto;
