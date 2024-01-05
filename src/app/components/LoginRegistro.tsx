a5ac883cb62a767f4ec06b85a1565c56966a6793
VictorEyeEagle <victor100bol@hotmail.com>
2024-01-05 20:01:13 -0300

    .

--- src/app/components/LoginRegistro.tsx
+++ src/app/components/LoginRegistro.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import React, { useState } from "react";
 import "./estilos/loginregistro.css";
 
@@ -7,20 +8,22 @@ function Loginregistro() {
 
     return (
         <div className="loginregistro">
-            <button onClick={() => setShow('entrar')} className="botao"><h2>Entrar</h2></button>
-            <button onClick={() => setShow('registrar')} className="botao"><h2>Registrar</h2></button>
+            <button onClick={() => setShow('entrar')} className={show === 'entrar' ? "botao ativo" : "botao"}><h2>Entrar</h2></button>
+            <button onClick={() => setShow('registrar')} className={show === 'registrar' ? "botao ativo" : "botao"}><h2>Registrar</h2></button>
             {show === 'entrar' && (
                 <div className="entrar" >
-                    <input id="email" placeholder="Email" className="input" />
-                    <input id="senha" placeholder="Senha" className="input" />
+                    <input type="email" id="email" placeholder="Email" className="input" />
+                    <input type="password" id="senha" placeholder="Senha" className="input" />
+                    <button><h2>Logar</h2></button>
                 </div>
             )}
             {show === 'registrar' && (
                 <div className="registrar">
-                    <input id="email" placeholder="Email" className="input" />
-                    <input id="senha" placeholder="Senha" className="input" />
                     <input id="nome de usuario" placeholder="Nome de Usuario" className="input" />
-                    <input id="confirmar senha" placeholder="Confirmar senha" className="input" />
+                    <input type="email" id="email" placeholder="Email" className="input" />
+                    <input type="password" id="senha" placeholder="Senha" className="input" />
+                    <input type="password" id="confirmar senha" placeholder="Confirmar senha" className="input" />
+                    <button><h2>Registrar-se</h2></button>
                 </div>
             )}
         </div>
