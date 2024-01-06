88bf996d8f32741c2d610385e8cc1d050765038b
VictorEyeEagle <victor100bol@hotmail.com>
2024-01-06 15:59:38 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Header from './components/header'
-import Inicio from './components/categorias'
+import Categorias from './components/categorias'
 import Tendencia from './components/tendencia'
 
 function Home() {
   return (
     <div className="container">
       <Header />
-      <Inicio />
+      <Categorias />
       <Tendencia />
     </div>
   )
